c3b0f38c53d4eb17246c07511f3e0ce0bac734df
tduckie_ <golden4gnvx@gmail.com>
2024-05-27 21:09:49 +0700

    adding axios post

--- src/components/input-component/Input.js
+++ src/components/input-component/Input.js
@@ -4,14 +4,20 @@ import "../style/Input.css";
 import { THEME, ThemeContext } from "../style/theme";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo, editTodo, getEditId } from "../store/todoListActions";
+import axios from "axios";
 
 const Input = (props) => {
   const [value, setValue] = useState("");
-  const todoDispatch = useDispatch();
+
+  const todoList = useSelector((state) => state.todoList);
   const editId = useSelector((state) => state.editId);
+
+  const todoDispatch = useDispatch();
   const { placeholder, inputRef } = props;
 
   const { theme } = useContext(ThemeContext);
+
+  const endpoint = "https://6652c3c6813d78e6d6d62e09.mockapi.io/todoList";
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +25,16 @@ const Input = (props) => {
       todoDispatch(editTodo(value));
       todoDispatch(getEditId({ id: null }));
     } else {
-      todoDispatch(addTodo(value));
+      axios
+        .post(endpoint, {
+          name: value,
+          id: todoList.length + 1,
+          status: false,
+        })
+        .then((res) => {
+          todoDispatch(addTodo(value));
+        })
+        .catch((err) => console.log(err));
     }
     setValue("");
   };
